ac42deb7dc0fe6db742162fd55d2c84783d42ac0
github username <github email address>
2019-04-15 16:14:55 -0400

    Still working on the model. Needed to switch branches for code

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -2,4 +2,14 @@ class Meetup < ApplicationRecord
   has_many :user_meetups
   belongs_to :campaign
   has_many :characters, through: :campaign
+
+  validates :campaign_id, presence: true
+  validates :location, presence: true
+  validates :number_of_hours, numericality: true
+
+  def format_meet_time
+
+  end
+
+  def
 end
